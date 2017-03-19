e2f3b835568f603afeda173141170b7ffd8de518
Choi Won <help@myso.kr>
2017-03-19 20:21:04 +0900

    Added disconnect test

--- test/index.js
+++ test/index.js
@@ -3,3 +3,4 @@ import Module from '../src/.';
 const module = new Module(null, null, 'http://play.afreecatv.com/aflol/190153440');
 module.on('message', (data)=>console.info(JSON.stringify(data)))
 module.connect();
+setTimeout(()=>module.disconnect(), 5000);
